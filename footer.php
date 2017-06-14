4ba0387abfc3185553c7ab1140abd934673667a1
ismael <ismaelfm_@ciencias.unam.mx>
2017-06-13 22:38:50 -0500

    Adding missing logos

--- footer.php
+++ footer.php
@@ -11,40 +11,7 @@
       <table class="wsite-multicol-table">
         <tbody class="wsite-multicol-tbody">
           <tr class="wsite-multicol-tr">
-            <td class="wsite-multicol-col" style="width:14.285714285715%; padding:0 15px;">
-              <div>
-                <div class="wsite-image wsite-image-border-none " style="padding-top:10px;padding-bottom:10px;margin-left:0px;margin-right:0px;text-align:right">
-                  <a href="http://www.fomin.org/es-es/" target="_blank">
-                    <img src="img/fomin.png" alt="FOMIN" style="width:auto;max-width:100%">
-                  </a>
-                  <div style="display:block;font-size:90%">
-                  </div>
-                </div>
-              </div>
-            </td>
-            <td class="wsite-multicol-col" style="width:14.285714285715%; padding:0 15px;">
-              <div>
-                <div class="wsite-image wsite-image-border-none " style="padding-top:10px;padding-bottom:10px;margin-left:0px;margin-right:0px;text-align:center">
-                  <a href="http://www.iadb.org/es/banco-interamericano-de-desarrollo,2837.html" target="_blank">
-                    <img src="img/BID.png" alt="BID" style="width:auto;max-width:100%">
-                  </a>
-                  <div style="display:block;font-size:90%">
-                  </div>
-                </div>
-              </div>
-            </td>
-            <td class="wsite-multicol-col" style="width:14.285714285715%; padding:0 15px;">
-              <div>
-                <div class="wsite-image wsite-image-border-none " style="padding-top:40px;padding-bottom:10px;margin-left:0px;margin-right:0px;text-align:center">
-                  <a href="http://pegaso.anahuac.mx/idearse" target="_blank">
-                    <img src="img/idear.png" alt="Idearse" style="width:auto;max-width:100%">
-                  </a>
-                  <div style="display:block;font-size:90%">
-                  </div>
-                </div>
-              </div>
-            </td>
-            <td class="wsite-multicol-col" style="width:14.285714285715%; padding:0 15px;">
+          <td class="wsite-multicol-col" style="width:12.5%; padding:0 15px;">
               <div>
                 <div class="wsite-image wsite-image-border-none " style="padding-top:10px;padding-bottom:10px;margin-left:0px;margin-right:0px;text-align:center">
                   <a href="http://www.nafin.com/portalnf/content/home/home.html" target="_blank">
@@ -55,22 +22,77 @@
                 </div>
               </div>
             </td>
-            <td class="wsite-multicol-col" style="width:14.285714285714%; padding:0 15px;">
+            <td class="wsite-multicol-col" style="width:12.5%; padding:0 15px;">
               <div>
                 <div class="wsite-image wsite-image-border-none " style="padding-top:10px;padding-bottom:10px;margin-left:0px;margin-right:0px;text-align:center">
-                  <a href="https://www.inadem.gob.mx" target="_blank">
-                    <img src="img/inadem.png" alt="INADEM" style="width:auto;max-width:100%">
+                  <a href="http://www.iadb.org/es/banco-interamericano-de-desarrollo,2837.html" target="_blank">
+                    <img src="img/BID.png" alt="BID" style="width:auto;max-width:100%">
                   </a>
                   <div style="display:block;font-size:90%">
                   </div>
                 </div>
               </div>
             </td>
-            <td class="wsite-multicol-col" style="width:14.285714285714%; padding:0 15px;">
+            <td class="wsite-multicol-col" style="width:12.5%; padding:0 15px;">
+              <div>
+                <div class="wsite-image wsite-image-border-none " style="padding-top:10px;padding-bottom:10px;margin-left:0px;margin-right:0px;text-align:right">
+                  <a href="http://www.fomin.org/es-es/" target="_blank">
+                    <img src="img/fomin.png" alt="FOMIN" style="width:auto;max-width:100%">
+                  </a>
+                  <div style="display:block;font-size:90%">
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td class="wsite-multicol-col" style="width:12.5%; padding:0 15px;">
               <div>
                 <div class="wsite-image wsite-image-border-none " style="padding-top:10px;padding-bottom:10px;margin-left:0px;margin-right:0px;text-align:center">
                   <a href="http://afico.org" target="_blank">
                     <img src="img/afico.png" alt="AFICO" style="width:auto;max-width:100%">
+                  </a>
+                  <div style="display:block;font-size:90%">
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td class="wsite-multicol-col" style="width:12.5%; padding:0 15px;">
+              <div>
+                <div class="wsite-image wsite-image-border-none " style="padding-top:10px;padding-bottom:10px;margin-left:0px;margin-right:0px;text-align:center">
+                  <a href="." target="_blank">
+                    <img src="img/crowdfunding.png" alt="CROWDFUNDING" style="width:auto;max-width:100%">
+                  </a>
+                  <div style="display:block;font-size:90%">
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td class="wsite-multicol-col" style="width:12.5%; padding:0 15px;">
+              <div>
+                <div class="wsite-image wsite-image-border-none " style="padding-top:10px;padding-bottom:10px;margin-left:0px;margin-right:0px;text-align:center">
+                  <a href="https://www.inadem.gob.mx" target="_blank">
+                    <img src="img/se_inadem.png" alt="INADEM" style="width:auto;max-width:100%">
+                  </a>
+                  <div style="display:block;font-size:90%">
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td class="wsite-multicol-col" style="width:12.5%; padding:0 15px;">
+              <div>
+                <div class="wsite-image wsite-image-border-none " style="padding-top:20px;padding-bottom:10px;margin-left:0px;margin-right:0px;text-align:center">
+                  <a href="http://pegaso.anahuac.mx/idearse" target="_blank">
+                    <img src="img/idear.png" alt="Idearse" style="width:auto;max-width:100%">
+                  </a>
+                  <div style="display:block;font-size:90%">
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td class="wsite-multicol-col" style="width:12.5%; padding:0 15px;">
+              <div>
+                <div class="wsite-image wsite-image-border-none " style="padding-top:10px;padding-bottom:10px;margin-left:0px;margin-right:0px;text-align:center">
+                  <a href="." target="_blank">
+                    <img src="img/mx_d-logos1.png" alt="MX_D" style="width:auto;max-width:100%">
                   </a>
                   <div style="display:block;font-size:90%">
                   </div>
