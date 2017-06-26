7886300600513cb743c4f75a3817b12a17764d4f
mvilchis <mvilchis@ciencias.unam.mx>
2017-06-25 23:04:06 -0500

    update script

--- plataformacf/clean_data.sh
+++ plataformacf/clean_data.sh
@@ -5,15 +5,16 @@ INDICADORES=("i111" "i11" "i121" "i12" "i131" "i13" "i14" "i15" "i16" "i17" "i18
 
 for indicador in "${INDICADORES[@]}"
 do
-  cat $PATH_RAW| awk -v FS="," -v indicador_copy="$indicador" '$3 ~ indicador_copy {print $0}' |sed "s/\"//g" > tmp.csv;
+  cat $PATH_RAW| awk -v FS="," -v indicador_copy="$indicador" '$1 ~ indicador_copy {print $0}' |sort -k 3,3 |sed "s/\"//g" > tmp.csv;
   cat  tmp.csv | \
   jq --slurp --raw-input --raw-output \
       'split("\n") | .[0:-1] | map(split(",")) |
-          map({"t": .[0],
-               "m": .[1],
-               "id": .[2],
-               "id2": .[3],
-               "cve": .[4],
-               "valor": .[5],
-               "DesGeo":.[6]})' > $PATH_OUTPUT"$indicador.json"
+          map({"id": .[0],
+               "cve": .[1],
+               "t": .[2],
+               "valor": .[3],
+               "m": .[4],
+               "id2": .[5],
+               "DesGeo": .[6],
+                })' > $PATH_OUTPUT"$indicador.json"
 done
