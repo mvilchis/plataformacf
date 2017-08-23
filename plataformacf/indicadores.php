bc3567be2b4a19bd0ff15549ea2760b0c1854672
mvilchis <mvilchis@ciencias.unam.mx>
2017-08-23 18:57:04 -0500

    Use gobmx design indicadores

--- plataformacf/indicadores.php
+++ plataformacf/indicadores.php
@@ -1,90 +1,92 @@
 <!DOCTYPE html>
-<html lang="es" class="no-js js gr__localhost">
-   <head profile="http://www.w3.org/1999/xhtml/vocab">
-      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="canonical" href="indicadores">
-      <title>Indicadores | Objetivos de Desarrollo Sostenible</title>
-      <!--                          Style                                    -->
-      <link rel="stylesheet" type="text/css" href="css/system.base.css"/>
-      <link rel="stylesheet" type="text/css" href="css/field.css"/>
-      <link rel="stylesheet" type="text/css" href="css/views.css"/>
-      <link rel="stylesheet" type="text/css" href="css/ctools.css"/>
-      <link rel="stylesheet" type="text/css" href="css/panels.css"/>
-      <link rel="stylesheet" type="text/css" href="css/field.css"/>
-      <link rel="stylesheet" type="text/css" href="css/flexible.css"/>
-      <link rel="stylesheet" type="text/css" href="css/64d7d0a3c55866afec0187d45d6e1cfe.css"/>
-      <link rel="stylesheet" type="text/css" href="css/jquery.magnific-popup.css"/>
-      <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
-      <link rel="stylesheet" type="text/css" href="css/overrides.css"/>
-      <link rel="stylesheet" type="text/css" href="css/common.css"/>
-      <link rel="stylesheet" type="text/css" href="css/style.css"/>
-      <link rel="stylesheet" type="text/css" href="css/css">
-      <link rel="stylesheet" type="text/css" href="css/critical.css" />
-      <link rel="stylesheet" type="text/css" href="css/plataformacf.css" />
-      <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans:400"/>
-      <!--                      script                                       -->
-      <script src="js/jquery.min.js"></script>
-      <script src="js/jquery.once.js"></script>
-      <script src="js/bootstrap.min.js"></script>
-      <script src="js/jquery.magnific-popup.min.js"></script>
-      <script src="js/jquery.matchHeight-min.js"></script>
-      <?=include_once('header.php');?>
-   </head>
-   <body class="html not-front not-logged-in no-sidebars page-node page-node- page-node-7 node-type-page navbar-is-fixed-top bootstrap-anchors-processed" data-gr-c-s-loaded="true">
-      <section class="main">
-         <div class="container">
-         <div class="row">
-         <section class="main-col col-md-12">
-            <div class="region region-content">
-            <section id="block-system-main" class="block block-system clearfix">
-               <div id="node-7" class="node node-page clearfix" about="/indicadores" typeof="foaf:Document">
-               <span property="dc:title" content="Indicadores" class="rdf-meta element-hidden"></span><span property="sioc:num_replies" content="0" datatype="xsd:integer" class="rdf-meta element-hidden"></span>
-               <div class="content">
-                  <div class="field field-name-body field-type-text-with-summary field-label-hidden">
-                     <div class="field-items">
-                        <div class="field-item even" property="content:encoded">
-                           <div class="row">
-                              <div class="col-xs-12">
-                                 Consulta datos de más de 100 indicadores correspondientes a los Objetivos de Desarrollo Sostenible.<br>Primero selecciona un objetivo, y posteriormente elige un indicador:
-                              </div>
-                           </div>
-                           <?php
-                              include('h_objetivos.php');
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Inicio | Sistema de monitoreo de fondeo colectivo en México</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width">
+  <link href="https://framework-gb.cdn.gob.mx/assets/styles/main.css" rel="stylesheet">
+  <link rel="shortcut icon" href="https://framework-gb.cdn.gob.mx/favicon.ico">
+  <link rel="stylesheet" type="text/css" href="css/plataformacf.css" />
+  <link href="https://framework-gb.cdn.gob.mx/assets/styles/main.css" rel="stylesheet">
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 
-                              $result = file_get_contents("json/cf_metadata.json");
-                              $metadata = json_decode($result, true);
-                              $indicadores_id = array();
-                              foreach($metadata["results"] as $value) {
-                              	if (array_key_exists($value["Nombre_del_objetivo"],$indicadores)) array_push($indicadores[$value["Nombre_del_objetivo"]],$value);
-                              	$indicadores_id[$value["Clave"]] = $value;
-                              }
-
-                              $i = 0;
-                              foreach($indicadores as $key => $objetivo) {
-                              	echo ('<div class="row indicador-header"><div class="col-xs-1" style="padding-top: 15px;"><img src="img/'.$objetivo_icons[$key].'.png"/></div><div class="col-xs-11"><h4><strong>'.($i+1).'.</strong> '.$objetivo_nombres[$key].'</h4></div></div>');
-                              	foreach($objetivo as $indicador) {
-                              		echo ( '<div class="row indicador-page-row" onmousedown="visit_indicador(\''.$i.'\',\''.$indicador["Clave"].'\')" ><div class="col-xs-1"></div><div class="col-xs-11">'.$indicador["Nombre_del_indicador"]." : ".$indicador["Descripcion"]."</div></div>" );
-                              	}
-                              	$i++;
-                              }
-
-                              ?>
-                           <script type="text/javascript">
-                              function visit_indicador(o,i) {
-                              	window.location.href='explora?o='+o+'&i='+i;
-                              }
-                           </script>
-                        </div>
-            </section>
-            <!-- /.block -->
-            </div>
-         </section>
-         </div>
-         </div>
-      </section>
-      <?php include("footer.php"); ?>
-      <script src="js/bootstrap.min.js"></script>
+  <link rel="import" href="bower_components/polymer/polymer.html">
+  <link rel="import" href="https://cdn.datos.gob.mx/bower_components/dgm-navbar/dgm-navbar.html">
+  <link rel="import" href="https://cdn.datos.gob.mx/bower_components/dgm-footer/dgm-footer.html">
+  <script type="text/javascript" src="bower_components/webcomponentsjs/webcomponents-lite.min.js"></script>
+</head>
+<body>
+  <!-- Crowdfunding bar  -->
+  <nav class=" navbar-crowdfunding navbar2 navbar-default navbar-fixed-top ">
+    <div class="container container-title">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="index.php">
+          <span>Crowdfunding Mx</span>
+        </a>
+      </div>
+      <div class="collapse navbar-collapse" id="crowdfunding-navbar">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="explora.php">Explora</a></li>
+          <li><a href="indicadores.php">Indicadores</a></li>
+          <li><a href="acerca.php">Acerca</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- /Crowdfunding end bar -->
+  <div>
+    <div class="container" style="padding-top:60px;">
+      <div class="col-sm-12">
+        <div>
+          <h2><b>Indicadores</b></h2>
+        </div>
+      </div>
+      <div class="col-sm-12 texto_parrafo" >
+    <?php
+    include('h_objetivos.php');
+    $result = file_get_contents("json/cf_metadata.json");
+    $metadata = json_decode($result, true);
+    $indicadores_id = array();
+    foreach($metadata["results"] as $value) {
+    	if (array_key_exists($value["Nombre_del_objetivo"],$indicadores)) array_push($indicadores[$value["Nombre_del_objetivo"]],$value);
+    	$indicadores_id[$value["Clave"]] = $value;
+    }
+    $i = 0;
+    foreach($indicadores as $key => $objetivo) {
+    	echo ('<div class="row indicador-header">
+              <div class="col-xs-1" style="padding-top: 15px;">
+                <img src="img/'.$objetivo_icons[$key].'.png"/>
+              </div>
+              <div class="col-xs-11">
+                <h4><strong>'.($i+1).'.</strong> '.$objetivo_nombres[$key].'</h4>
+              </div>
+            </div>');
+  	  foreach($objetivo as $indicador) {
+        echo ( '<div class="row indicador-page-row" onmousedown="visit_indicador(\''.$i.'\',\''.$indicador["Clave"].'\')" >
+                  <div class="col-xs-1"></div>
+                  <div class="col-xs-11">'.$indicador["Nombre_del_indicador"]." : ".$indicador["Descripcion"]."
+                  </div>
+                </div>" );
+  	  }
+  	  $i++;
+    }
+  ?>
+      </div>
+    </div>
+  </div>
+  <script src="bower_components/jquery/dist/jquery.js"></script>
+  <script src="https://framework-gb.cdn.gob.mx/gobmx.js"></script>
+  <script src="bower_components/underscore/underscore-min.js"></script>
+  <script src="bower_components/bootstrap-year-calendar/js/bootstrap-year-calendar.min.js"></script>
+  <script src="bower_components/moment/min/moment.min.js"></script>
+  <script src="bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script type="text/javascript">
+   function visit_indicador(o,i) {
+     window.location.href='explora?o='+o+'&i='+i;
+    }
+  </script>
+  <?php include("acknowledgment.php"); ?>
    </body>
 </html>
