84f03468a20ad46f296fc2d894826d3b1926b428
mvilchis <mvilchis@ciencias.unam.mx>
2017-11-28 10:31:50 -0600

    Change acerca description

--- plataformacf/acerca.php
+++ plataformacf/acerca.php
@@ -38,39 +38,23 @@
   <div>
   <div class="container" style="padding-top:60px;">
     <div class="col-sm-12 texto_parrafo" property="content:encoded">
+    <h2>Sistema de Monitoreo de Fondeo Colectivo</h2>
+    <div class="">
+      <p>Es una iniciativa de Crowdfunding México que permite visualizar y descargar información en tiempo real sobre los cuatro tipos principales de fondeo colectivo en México: Capital, Deuda, Donaciones y Recompensa. </p>
+      <p>El objetivo principal del Sistema es impulsar el ecosistema de fondeo colectivo a través de un mejor entendimiento de sus actores y las principales variables que los afectan. Un mayor conocimiento del estado del fondeo colectivo en México puede además generar sinergias entre sus jugadores e impulsar un mercado satélite de actores como respuesta a la existencia de un ecosistema creciente.</p>
+      <p>A la fecha, la plataforma incluye datos del 80% de los proyectos de fondeo colectivo en México y se está trabajando para que más plataformas se unan todos los días. </p>
+    </div>
+
     <h2>¿Quién es Crowdfunding México?</h2>
     <div class="">
-    <p>Es una iniciativa del Fondo Multilateral de Inversiones (FOMIN) del Banco Interamericano de Desarrollo (BID) ejecutado por la Universidad Anáhuac a través del Centro IDEARSE con la participación de Nacional Financiera, el Instituto Nacional del Emprendedor, la Oficina de Estrategia Digital de la Presidencia de la República, la Comisión Nacional Bancaria y de Valores y la Asociación de Plataformas de Fondeo Colectivo, para impulsar proyectos vía crowdfunding o de financiamiento/fondeo colectivo, ecosistema que está en una fase inicial en México, y que es considerado un mercado alternativo de fondeo para proyectos innovadores a través de plataformas digitales.</p>
+    <p>Es una iniciativa del Fondo Multilateral de Inversiones (FOMIN) del Banco Interamericano de Desarrollo (BID) ejecutado por la Universidad Anáhuac a través del Centro IDEARSE con la participación de Nacional Financiera (NAFIN), el Instituto Nacional del Emprendedor (INADEM), la Oficina de Estrategia Digital de la Presidencia de la República, la Comisión Nacional Bancaria y de Valores (CNBV) y la Asociación de Plataformas de Fondeo Colectivo (AFICO), para impulsar proyectos vía crowdfunding o de financiamiento/fondeo colectivo, ecosistema que está en una etapa inicial en México, y que es considerado un mercado alternativo de fondeo para proyectos innovadores a través de plataformas digitales. </p>
     </div>
 
     <h2>¿Cómo funciona Crowdfunding México?</h2>
     <div class="">
-    <p>El proyecto reforzará el ecosistema de financiamiento colectivo mediante la consolidación de la Asociación de Plataformas de Fondeo Colectivo (AFICO), liderada por las principales plataformas de financiamiento colectivo de México. Ésta recibirá asistencia técnica para colaborar con agentes clave a fin de facilitar el diálogo para estudiar los parámetros de un marco prudencial propicio para la aceleración del desarrollo del ecosistema. Ese apoyo se basará en prácticas óptimas reconocidas en materia de financiamiento colectivo, según la experiencia de mercados desarrollados, y velará por que se considere adecuadamente el contexto local. Los conocimientos generados en la puesta a prueba de este nuevo modelo de intervención serán extraordinariamente valiosos para llenar los vacíos de conocimiento actuales y diseñar iniciativas de financiamiento colectivo similares en otros países. </p>
-    </div>
-
-    <h2>Aceleración del ecosistema de financiamiento colectivo</h2>
-    <div class="">
-    <p>El proyecto pondrá a prueba un modelo de intervención colaborativa enfocado en acelerar el desarrollo de un ecosistema de financiamiento colectivo sostenible en México. En consonancia con las recomendaciones del informe sobre financiamiento colectivo en México, el modelo cubrirá la brecha entre la demanda de financiamiento de los empresarios y las MIPYME, el entendimiento del mercado y las fuentes de financiamiento. Al fortalecer las capacidades de las plataformas de financiamiento colectivo existentes y crear un entorno propicio para el surgimiento de nuevas plataformas, permitirá que los empresarios y las MIPYME las aprovechen mejor y tengan acceso a una multitud de nuevas oportunidades de negocios.</p>
-    </div>
-
-    <h2>Asistencia Técnica</h2>
-    <div class="">
-    <p>El proyecto línea, brindará a empresas y emprendedores locales (en especial, empresas sociales y negocios liderados por mujeres y jóvenes) asistencia técnica que mejorará su capacidad para utilizar tecnologías digitales y gestionar operaciones comerciales. Pondrá en práctica una estrategia de divulgación incluyente focalizada en los beneficiarios por medio de asociaciones de pequeñas empresas, redes afiliadas de contrapartes del proyecto (por ejemplo, la Red de Apoyo al Emprendedor del INADEM), y estudiantes universitarios. Además, con miras a maximizar su alcance y garantizar iguales oportunidades de formación, el proyecto creará herramientas de capacitación a las que se podrá acceder en línea.</p>
-    </div>
-
-    <h2>Capacitación</h2>
-    <div class="">
-    <p>El proyecto también ofrecerá capacitación a los microinversionistas potenciales, a efecto de mejorar su capacidad para identificar y evaluar las oportunidades de financiamiento colectivo basado en donaciones, recompensas y préstamos, y los riesgos pertinentes. Es posible que también se imparta capacitación avanzada a inversionistas acreditados, con miras a informarles acerca de las plataformas de financiamiento colectivo basado en acciones, las oportunidades actuales y las limitaciones y riesgos. En todas las actividades de capacitación se pondrá énfasis especial en las consideraciones pertinentes para apoyar con financiamiento colectivo financiero y no financiero a las empresas sociales y los negocios liderados por mujeres o jóvenes, o propiedad de mujeres o jóvenes. Con objeto de maximizar el acceso, también se pondrá a disposición de los microinversionistas material en línea.</p>
-    <h2>Beneficios del Crowdfunding México</h2>
-    </div>
-
-    <div class="">
-    <p>El proyecto prevé que se operen diversos cambios en el comportamiento de los empresarios y las MIPYME. Las actividades educativas y promocionales, aunadas a la elaboración y diseminación de materiales didácticos pertinentes, fomentarán el uso de las plataformas de financiamiento colectivo entre los empresarios y los microinversionistas.</p>
-    </div>
-
-    <h2>Sistema de Monitoreo</h2>
-    <div class="">
-    <p>El proyecto prevé que se operen diversos cambios en el comportamiento de los empresarios y las MIPYME. Las actividades educativas y promocionales, aunadas a la elaboración y diseminación de materiales didácticos pertinentes, fomentarán el uso de las plataformas de financiamiento colectivo entre los empresarios y los microinversionistas.</p>
+      <p>El proyecto busca reforzar el ecosistema de financiamiento colectivo mediante la consolidación de la Asociación de Plataformas de Fondeo Colectivo (AFICO), liderada por las principales plataformas de financiamiento colectivo de México. </p>
+      <p>El proyecto prevé que se  generen diversos cambios en el comportamiento de los empresarios y las MIPYME, mediante actividades educativas y promocionales, aunadas a la elaboración y diseminación de materiales didácticos pertinentes, fomentarán el uso de las plataformas de financiamiento colectivo entre los empresarios y los microinversionistas.</p>
+      <p>Además, a través de la AFICO  se ha buscado recopilar información valiosa de las diversas plataformas de fondeo colectivo en México, con el fin de generar estadísticas confiables y buenas prácticas dentro del ecosistema, que a su vez generen la confianza necesaria para que los usuarios sigan utilizando estos nuevos mecanismos de financiamiento.</p>
     </div>
     </div>
   </div>
